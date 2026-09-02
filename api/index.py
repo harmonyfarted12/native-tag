c50ff5931c39387d525ca5b64272c52e5b46216e
harmonyfarted12 <r6acc13224@gmail.com>
2026-09-02 08:15:15 -0800

    Update index.py

--- api/index.py
+++ api/index.py
@@ -571,8 +571,8 @@ class GameInfo:
        self.SecretKey = "HM4GOGINZORY95ZJHQKABNRCFZHRTPCYK4R1SRJ8F885FEYHM7"
        self.AppCreds = "OC|1358888013967187|0ec4b351f6d1d95f563f4e8bb76b9bd2"
        self.OculusAppId = "1358888013967187"
-       self.EntitlementCheck = false
-       self.appidcheck = false
+       self.EntitlementCheck = False
+       self.appidcheck = True
        self.MaxAllowedSpeed = 9
        self.MaxAllowedArmLength = 1.45
        self.SpeedViolationThreshold = 3
